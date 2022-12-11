8d92fe0d0ac74c0d64e3442a10d6e7b5bf95158d
= <tomas.gonzalez@mercadolibre.com.co>
2022-12-11 00:00:27 -0500

    agregando lineas

--- scripts/Main.java
+++ scripts/Main.java
@@ -1,7 +1,8 @@
 public class Main{
 
     public static void main(String[] args) {
-        System.out.println("Tomas");
+        System.out.println("Tomas Gonzalez");
+        System.out.println("Hola mundo");
     }
 
 }
