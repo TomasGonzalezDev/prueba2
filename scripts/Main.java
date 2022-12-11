38b1c597190d37cde947615a2f05d29dcc21a939
= <tomas.gonzalez@mercadolibre.com.co>
2022-12-11 00:06:55 -0500

    annadiendo lineas main

--- scripts/Main.java
+++ scripts/Main.java
@@ -1,7 +1,11 @@
 public class Main{
 
     public static void main(String[] args) {
-        System.out.println("Tomas");
+        System.out.println("Tomas Romero");
+    }
+
+    static void Hola(){
+        System.out.println("Hola");
     }
 
 }
